d369cf9a28044e23ec8b9bab1d80aef92d78d78e
tomas-loera <tomasloera2003@gmail.com>
2026-09-22 22:56:12 -0600

    Se añadieron mejoras ortográficas al bloque 1 y 3. Se añadió el bloque 2 el cual concatena los datos obtenidos en el bloque 1. Se añadieron los siguientes cambios al bloque 5: Mejoras estéticas. Un resumen de los datos capturados. El resultado redondeado a dos decimales.

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,16 +1,27 @@
-#Se espera que estas tres primeras variables sean del tipo string
-nombre = input('¿Cuál es tu nombre?')
-apellido_paterno = input('¿Cuál es tu apellido paterno?')
-apellido_materno = input('¿Cuál es tu apellido materno?')
+#En este primer bloque se solicita el nombre completo del usuario.
+#Se espera que todos sean del tipo string
+nombre = input('¿Cuál es tu nombre(s)? ')
+apellido_paterno = input('¿Cuál es tu apellido paterno? ')
+apellido_materno = input('¿Cuál es tu apellido materno? ')
 
+#Esta variable concatena los tres datos solicitados en el bloque anterior
+nombre_completo = nombre, apellido_paterno, apellido_materno
 
+#En este segundo bloque se solicitan los datos numéricos
+edad = int(input('¿Cuál  es tu edad? '))#Se hace la conversión a entero
+peso = int(input('¿Cuál es tu peso? '))#Se hace la conversión a entero
+estatura = float(input('¿Cuál es tu estatura? '))#Se hace la conversión a float
 
-edad = int(input('¿Cuál es tu edad?')) #Se espera que esta variable sea de tipo entero, por eso se hace la conversión
-peso = float(input('¿Cuál es tu peso?')) #Se espera que esta variable sea de tipo float, por eso se hace la conversión
-estatura = float(input('¿Cuál es tu estatura?')) #Se espera que esta variable sea de tipo float, por eso se hace la conversión
-
+#Se hace el calculo del IMC
 imc=peso/estatura**2
 
-print(f'{nombre} {apellido_paterno} {apellido_materno}, tu IMC es: {imc}. Gracias por usar esta calculadora.')
-
-
+#Se imprime el resultado
+print(f"""
++--------------------+
+{" ".join(nombre_completo).title()}
+Edad: {edad}
+Estatura: {estatura}
+Peso: {peso}\nIMC: {imc:.2f}
+Gracias por usar esta calculadora.
++--------------------+
+""")
