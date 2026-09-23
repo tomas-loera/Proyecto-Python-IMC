947ffbcc70e45bdd229926a075d3246901b21176
tomas-loera <tomasloera2003@gmail.com>
2026-09-22 23:01:22 -0600

    Se añadieron mejoras ortográficas al bloque 1 y 3. Se añadió el bloque 2 el cual concatena los datos obtenidos en el bloque 1. Se añadieron los siguientes cambios al bloque 5: Mejoras estéticas. Un resumen de los datos capturados. El resultado redondeado a dos decimales.

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,21 +1,21 @@
-#En este primer bloque se solicita el nombre completo del usuario.
+#Bloque 1: En este primer bloque se solicita el nombre completo del usuario.
 #Se espera que todos sean del tipo string
 nombre = input('¿Cuál es tu nombre(s)? ')
 apellido_paterno = input('¿Cuál es tu apellido paterno? ')
 apellido_materno = input('¿Cuál es tu apellido materno? ')
 
-#Esta variable concatena los tres datos solicitados en el bloque anterior
+#Bloque 2: Esta variable concatena los tres datos solicitados en el bloque anterior
 nombre_completo = nombre, apellido_paterno, apellido_materno
 
-#En este segundo bloque se solicitan los datos numéricos
+#Bloque 3: En este segundo bloque se solicitan los datos numéricos
 edad = int(input('¿Cuál  es tu edad? '))#Se hace la conversión a entero
 peso = int(input('¿Cuál es tu peso? '))#Se hace la conversión a entero
 estatura = float(input('¿Cuál es tu estatura? '))#Se hace la conversión a float
 
-#Se hace el calculo del IMC
+#Bloque 4: Se hace el calculo del IMC
 imc=peso/estatura**2
 
-#Se imprime el resultado
+#Bloque 5: Se imprime el resultado
 print(f"""
 +--------------------+
 {" ".join(nombre_completo).title()}
